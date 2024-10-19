8bb508f2824276a0656460e7f7c093446dcc1958
mohamedhassan <mohamed7github@gmail.com>
2024-10-19 16:53:28 +0300

    Add App Colors

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff8E6CEF);
+
+  static const fillColorLightMode = Color(0xffF4F4F4);
+  static const fillColorDarkMode = Color(0xff342F3F);
+
+  static const lightModeBackgroundColor = Color(0xffFFFFFF);
+  static const darkModeBackgroundColor = Color(0xff1D182A);
+
+  static const primaryTextColorLightMode = Color(0xff272727);
+  static const primaryTextColorDarkMode = Color(0xffFFFFFF);
+
+  static const hintTextColorLightMode = Color(0xff8E8E8E);
+  static const hintTextColorDarkMode = Color(0xff99979F);
+}
