ebf9511410625c6b80babbd51e43c68c393fdb27
mohamedhassan <mohamed7github@gmail.com>
2024-10-20 22:49:21 +0300

    add and initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:online_store/core/configs/theme/app_theme.dart';
 import 'package:online_store/features/splash/presentation/pages/splash_page.dart';
+import 'package:online_store/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApp(),
   );
