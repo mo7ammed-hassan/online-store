6795939c0afaf5d1440311a3748dcca7f8174acb
mohamedhassan <mohamed7github@gmail.com>
2024-10-20 23:06:42 +0300

    create UserCreationReqModel

---
+++ lib/features/auth/data/models/user_creation_req_model.dart
@@ -0,0 +1,14 @@
+
+class UserCreationReqModel {
+  String? firstName;
+  String? lastName;
+  String? email;
+  String? password;
+
+  UserCreationReqModel({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+  });
+}
