425cb658a067edfa5bef565b165386ff058758b3
mohamedhassan <mohamed7github@gmail.com>
2024-11-02 00:06:47 +0200

    create AddToCartReqModel

---
+++ lib/features/order/data/models/add_to_cart_req_model.dart
@@ -0,0 +1,37 @@
+class AddToCartReqModel {
+  final String productId;
+  final String productTitle;
+  final int productQuantity;
+  final String productColor;
+  final String productSize;
+  final double productPrice;
+  final double totalPrice;
+  final String productImageUrl;
+  final String createdDate;
+
+  AddToCartReqModel({
+    required this.productId,
+    required this.productTitle,
+    required this.productQuantity,
+    required this.productColor,
+    required this.productSize,
+    required this.productPrice,
+    required this.totalPrice,
+    required this.productImageUrl,
+    required this.createdDate,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'productId': productId,
+      'productTitle': productTitle,
+      'productQuantity': productQuantity,
+      'productColor': productColor,
+      'productSize': productSize,
+      'productPrice': productPrice,
+      'totalPrice': totalPrice,
+      'productImageUrl': productImageUrl,
+      'createdDate': createdDate,
+    };
+  }
+}
